1ab1523eb037828b9182e12733f08b905c148d61
Daniel Romero <dandres508@gmail.com>
2024-11-05 13:24:04 -0500

    Encontrar libro por nombre

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -6,12 +6,13 @@ import com.aluracursos.desafio.service.ConsumoAPI;
 import com.aluracursos.desafio.service.ConvierteDatos;
 
 import java.util.Comparator;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Principal {
     private final ConsumoAPI consumoApi = new ConsumoAPI();
     private final ConvierteDatos conversor = new ConvierteDatos();
-    private Scanner teclado = new Scanner(System.in);
+    private final Scanner teclado = new Scanner(System.in);
     private static final String API_URL = "https://gutendex.com/books/";
 
     public void muestraMenu() {
@@ -21,14 +22,29 @@ public class Principal {
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
 
-
+        //Top 10 de libros más descargados
         System.out.println("\nTop 10 de libros más descargados\n");
-
         datos.resultados().stream()
                 .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
                 .limit(10)
                 .map(l -> l.titulo().toUpperCase())
                 .forEach(System.out::println);
+
+        //Búsqueda de libros por nombre
+        System.out.println("\nPor favor escribe el comienzo del titulo del libro que deseas encontrar\n");
+        var cominezoTitulo = teclado.nextLine();
+        json = consumoApi.obtenerDatos(API_URL+"?search=" + cominezoTitulo.replace(" ", "+"));
+        var datosBusqueda = conversor.obtenerDatos(json, Datos.class);
+        Optional<DatosLibros> libroBuscado = datosBusqueda.resultados().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(cominezoTitulo.toUpperCase()))
+                .findFirst();
+        if (libroBuscado.isPresent()){
+            System.out.println("Libro encontrado ");
+            System.out.println(libroBuscado.get());
+        }else {
+            System.out.println("Libro no encontrado");
+        }
+
     }
 
 }
