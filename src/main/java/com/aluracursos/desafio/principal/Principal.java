8eccca7a8383528be3ee6426ada4d9f79c3e9fc3
Daniel Romero <dandres508@gmail.com>
2024-11-05 12:29:55 -0500

    Traer el top 10 de libros más descargados

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -1,23 +1,34 @@
 package com.aluracursos.desafio.principal;
 
 import com.aluracursos.desafio.model.Datos;
+import com.aluracursos.desafio.model.DatosLibros;
 import com.aluracursos.desafio.service.ConsumoAPI;
 import com.aluracursos.desafio.service.ConvierteDatos;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
     private final ConsumoAPI consumoApi = new ConsumoAPI();
     private final ConvierteDatos conversor = new ConvierteDatos();
-    private  Scanner teclado = new Scanner(System.in);
+    private Scanner teclado = new Scanner(System.in);
     private static final String API_URL = "https://gutendex.com/books/";
 
-    public void muestraMenu(){
+    public void muestraMenu() {
 
-        var json = consumoApi.obtenerDatos(API_URL );
+        var json = consumoApi.obtenerDatos(API_URL);
         System.out.println(json);
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+
+        System.out.println("\nTop 10 de libros más descargados\n");
+
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
     }
 
 }
